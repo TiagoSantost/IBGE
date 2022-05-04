410039e38340e2e5a31df562b1e8c0d12e507c98
TiagoSantost <89109699+TiagoSantost@users.noreply.github.com>
2022-05-04 16:57:18 -0300

    Add files via upload

---
+++ IBGE/script.js
@@ -0,0 +1,67 @@
+document.onreadystatechange = function(){
+    if(document.readyState == "complete"){
+        pegar_estado();
+        document.querySelector('#dados').onchange = mostrar_cidades;   
+       // document.getElementById("clique").addEventListener("click", pegar_micro);
+       // document.querySelector('#exibir').onchange = mostrar_micro; 
+    }
+}
+
+
+function pegar_estado(){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState === 4){
+            if(httpRequest.status === 200){
+                response = JSON.parse(httpRequest.responseText);
+
+               
+                var lista = document.querySelector('#dados');
+                lista.innerHTML = "";
+               
+                response.forEach(function (el){
+                option = document.createElement("option");      
+                option.innerHTML =  el.nome;
+                option.value = el.sigla;
+                option.setAttribute('nome', JSON.stringify (el.nome))
+                lista.appendChild(option);
+             })           
+        }
+    }
+}
+
+    httpRequest.open('GET', 'https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    httpRequest.send();
+}   
+
+
+
+function mostrar_cidades(){
+    sigla_estados = (document.querySelector('#dados').value)
+    
+    var httpRequest = new XMLHttpRequest();
+    
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState === 4){
+            if(httpRequest.status === 200){
+                response = JSON.parse(httpRequest.responseText);               
+                var lista = document.querySelector('#cidade');
+                lista.innerHTML = "";
+                response.forEach(function (el){
+                option = document.createElement("option");      
+                option.innerHTML =  el.nome
+                option.setAttribute('nome', JSON.stringify (el.nome))
+                lista.appendChild(option);
+            
+            })
+        }
+    }
+}
+
+    httpRequest.open('GET', 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+sigla_estados+'/municipios');
+    httpRequest.send();
+    
+}
+
+
